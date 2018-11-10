cbc1fc86d15b09741e067a8a5e7cf16c9ad1a7fc
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-11-10 16:45:03 +0700

    Revise app testing

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { MemoryRouter } from "react-router-dom";
-import { JssProvider, createGenerateClassName } from "react-jss";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 
 import App from "../App";
 
 describe("<App />", () => {
   test("renders without exploding", () => {
-    const theme = createMuiTheme({ typography: { useNextVariants: true } });
-    const generateClassName = createGenerateClassName();
-
     const div = document.createElement("div");
     render(
-      <JssProvider generateClassName={generateClassName}>
-        <MuiThemeProvider theme={theme}>
-          <MemoryRouter>
-            <App />
-          </MemoryRouter>
-        </MuiThemeProvider>
-      </JssProvider>,
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
       div,
     );
     unmountComponentAtNode(div);
